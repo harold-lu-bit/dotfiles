e13be8cdbe20d754e8b9aacd7c38fb8799a2275c
Harold Lu <harold_lu@foxmail.com>
2023-09-11 15:59:44 +0800

    去除.tmux，直接通过.tmux.conf添加一个简单的tmux主题

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_path=$(dirname "$(readlink -f "$0")")
 echo $script_path
-ln -sf $script_path/.tmux.conf.local ~/.tmux.conf.local
+ln -sf $script_path/.tmux.conf ~/.tmux.conf
 ln -sf $script_path/.zshrc ~/.zshrc
 ln -sf $script_path/.vimrc ~/.vimrc
 ln -sf $script_path/.aliases ~/.aliases
